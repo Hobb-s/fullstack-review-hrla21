e5df4d9c89981405a08cb75a71e16a46c09baab8
Warren <warren.p.kwong@gmail.com>
2018-04-07 17:37:28 -0700

    Setting up server + router; successfully serves up static files

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,23 @@
-// YOUR CODE HERE
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes =  require('./routes/routes.js');
+
+// Makes our server
+const app = express();
+const PORT = 3001;
+
+// Use middelware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Serve static files - to directory
+app.use(express.static(path.join(__dirname, '../../client')));
+
+app.use('/api', routes);
+
+app.use('*', (req, res) => res.status(404).send('This page does not exist'));
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}!`);
+})
---
+++ server/src/routes/routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+router.get('/playerslist', (req, res) => {});
+router.post('/submit-player', (req, res) => {});
+
+module.exports = router;
